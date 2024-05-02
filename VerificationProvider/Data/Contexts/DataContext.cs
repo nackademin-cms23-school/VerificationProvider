d2db870647f44b55a79103dba0619719a0960d6d
EmperorBlizzard <emil111@live.se>
2024-05-02 11:09:45 +0200

    Added db

---
+++ VerificationProvider/Data/Contexts/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerificationProvider.Data.Entities;
+
+namespace VerificationProvider.Data.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<VerificationRequestEntity> VerificationRequests { get; set; }
+}
--- VerificationProvider/Program.cs
+++ VerificationProvider/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VerificationProvider.Data.Contexts;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -8,7 +10,14 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
     })
     .Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
 
 host.Run();
